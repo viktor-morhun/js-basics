3619704379faf5ba5c857ad4e5611e48c2d7b05e
Viktor Morhun <morhun.fe@gmail.com>
2024-07-29 18:09:57 +0300

    age script updated

--- script.js
+++ script.js
@@ -3,15 +3,18 @@
 const USER_NAME = prompt("What is your name?");
 
 alert(`Hello, ${USER_NAME}`);
-
 yearCalculator();
 
 function yearCalculator (errorMessage = '') {
   const USER_BIRTHDATE = prompt(`Your birth year?${errorMessage}`);
   const CURRENT_YEAR = new Date().getFullYear();
 
-  if (USER_BIRTHDATE === `2001`) {
-    alert(`${USER_BIRTHDATE} is a good year!`);
+  if (+USER_BIRTHDATE >= 1900 && CURRENT_YEAR - +USER_BIRTHDATE >= 18) {
+    alert(`You are ${CURRENT_YEAR - +USER_BIRTHDATE} years old. Ok, you can read the article.`);
+  }
+  else if (CURRENT_YEAR - +USER_BIRTHDATE < 18) {
+    alert(`You are too young to view this content. Please come back when you are 18. Your current age is ${CURRENT_YEAR - +USER_BIRTHDATE}.`);
+    window.location.href = 'https://www.wikihow.com/Be-Older-Than-Your-Age';
   }
   else {
     yearCalculator(` Please enter a number between 1900 and ${CURRENT_YEAR}`)
