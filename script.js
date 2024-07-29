4c3dbd08ad99e0c72aa52a057ae926cfd970b8a1
Viktor Morhun <morhun.fe@gmail.com>
2024-07-29 15:57:46 +0300

    add name and year prompt

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const USER_NAME = prompt("What is your name?");
+
+alert(`Hello, ${USER_NAME}`);
+
+yearCalculator();
+
+function yearCalculator (errorMessage = '') {
+  const USER_BIRTHDATE = prompt(`Your birth year?${errorMessage}`);
+  const CURRENT_YEAR = new Date().getFullYear();
+
+  if (USER_BIRTHDATE === `2001`) {
+    alert(`${USER_BIRTHDATE} is a good year!`);
+  }
+  else {
+    yearCalculator(` Please enter a number between 1900 and ${CURRENT_YEAR}`)
+  }
+}
