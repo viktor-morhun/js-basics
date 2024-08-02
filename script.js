6c94a49ae32dd4eb236f8bd4a50b2e952b314681
Viktor Morhun <morhun.fe@gmail.com>
2024-08-02 11:30:27 +0300

    add visibility change event listener

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 'use strict';
 
-const USER_NAME = prompt("What is your name?");
-alert(`Hello, ${USER_NAME}`);
+document.addEventListener("visibilitychange", function() {
+  if (document.visibilityState === 'visible') {
+    startApp();
+  }
+});
 
-const SIDE_LENGTH = prompt("Enter the side length of the square to get the perimeter.");
-if (SIDE_LENGTH * 1 === +SIDE_LENGTH) {
-  alert(`The perimeter of a square with side ${SIDE_LENGTH} is ${SIDE_LENGTH * 4} units.`);
-} else {
-  alert(`Don't play with me. ${SIDE_LENGTH} is not a number!`);
+function startApp() {
+  const USER_NAME = prompt("What is your name?");
+  alert(`Hello, ${USER_NAME}`);
+
+  const SIDE_LENGTH = prompt("Enter the side length of the square to get the perimeter.");
+  if (SIDE_LENGTH * 1 === +SIDE_LENGTH) {
+    alert(`The perimeter of a square with side ${SIDE_LENGTH} is ${SIDE_LENGTH * 4} units.`);
+  } else {
+    alert(`Don't play with me. ${SIDE_LENGTH} is not a number!`);
+  }
+
+  yearCalculator();
 }
 
-yearCalculator();
-
-function yearCalculator (errorMessage = '') {
+function yearCalculator(errorMessage = '') {
   const USER_BIRTHDATE = prompt(`Your birth year?${errorMessage}`);
   const CURRENT_YEAR = new Date().getFullYear();
 
   if (+USER_BIRTHDATE >= 1900 && CURRENT_YEAR - +USER_BIRTHDATE >= 18) {
     alert(`You are ${CURRENT_YEAR - +USER_BIRTHDATE} years old. Ok, you can read the article.`);
-  }
-  else if (CURRENT_YEAR - +USER_BIRTHDATE < 18) {
+  } else if (CURRENT_YEAR - +USER_BIRTHDATE < 18) {
     alert(`You are too young to view this content. Please come back when you are 18. Your current age is ${CURRENT_YEAR - +USER_BIRTHDATE}.`);
     window.location.href = 'https://www.wikihow.com/Be-Older-Than-Your-Age';
-  }
-  else {
-    yearCalculator(` Please enter a number between 1900 and ${CURRENT_YEAR}`)
+  } else {
+    yearCalculator(` Please enter a number between 1900 and ${CURRENT_YEAR}`);
   }
 }
